8dd2a85650a8d18b7c9dc640754013b884c11709
facundo sura <facundomesura@gmail.com>
2025-10-08 18:10:06 -0300

    useEffect y use params de Detail.tsx borrado

--- app/components/Detail.tsx
+++ app/components/Detail.tsx
@@ -1,8 +1,7 @@
 'use client';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import { useParams } from 'next/navigation';
 import { PropiedadDetalle, VehiculoDetalle, ItemTipo } from '../types';
 import WhatsAppButton from './whatsappButton';
 
